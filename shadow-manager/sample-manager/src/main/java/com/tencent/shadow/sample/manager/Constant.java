922e0dd18efee0bc6b0735b1722b0d9732284c33
leigao9 <leigao9@iflytek.com>
2023-11-23 11:12:27 +0800

    feat: 增加shadow-manager

---
+++ shadow-manager/sample-manager/src/main/java/com/tencent/shadow/sample/manager/Constant.java
@@ -0,0 +1,15 @@
+package com.tencent.shadow.sample.manager;
+
+final public class Constant {
+    public static final String KEY_PLUGIN_ZIP_PATH = "pluginZipPath";
+    public static final String KEY_ACTIVITY_CLASSNAME = "KEY_ACTIVITY_CLASSNAME";
+    public static final String KEY_SERVICE_CLASSNAME = "KEY_SERVICE_CLASSNAME";
+    public static final String KEY_EXTRAS = "KEY_EXTRAS";
+    public static final String KEY_PLUGIN_PART_KEY = "KEY_PLUGIN_PART_KEY";
+    public static final String PART_KEY_PLUGIN_SAMPLE = "sample-plugin";
+    public static final String PART_KEY_PLUGIN_KYXLSTU = "kyxlstu-plugin";
+
+    public static final int FROM_ID_START_ACTIVITY = 1001;
+    public static final int FROM_ID_CALL_SERVICE = 1002;
+    public static final int FROM_ID_START_KYXL_ACTIVITY = 1003;
+}
